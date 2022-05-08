058eb3cd4d58df585b9f0d7e2cf1545c7b9e043f
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-08 17:56:32 +0200

    added movement to green bloops

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
+import react, { useState } from "react";
 import { Paper } from "@mui/material";
 import { ThemeProvider } from "@mui/material";
 import classicTheme from "./Themes/ClassicTheme";
+import "./dist/css/general.css";
 
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Blops from "./components/Blops";
 
 function App() {
+  let [mouse, setMouse] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  function getMousePosition(e) {
+    // console.log(e.clientX, e.clientY);
+    setMouse({
+      x: e.clientX,
+      y: e.clientY,
+    });
+    // console.log(mouse.x / window.innerWidth, mouse.y / window.innerHeight);
+  }
+
   return (
-    <div className="App">
-      <Blops />
+    <div className="App" onMouseMove={(e) => getMousePosition(e)}>
+      <Blops mouse={mouse} />
       <ThemeProvider theme={classicTheme}>
         <Paper
           elevation={0}
--- src/components/Blops.js
+++ src/components/Blops.js
@@ -4,16 +4,53 @@ import blopGreenMd from "../assets/green-blops/blop-green-md.svg";
 import blopGreenSm from "../assets/green-blops/blop-green-sm.svg";
 import "../dist/css/blops.css";
 
-export default function Blops() {
+export default function Blops({ mouse }) {
+  //animate blop position on mouse move
+  //   const [blopPosition, setBlopPosition] = react.useState({
+  //     x: 0,
+  //     y: 0,
+  //   });
+  // window size
+  let windowSize = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
+
   return (
     <div className="blops__container">
-      <div className="blop__container blop-lg">
+      <div
+        className="blop__container blop-lg"
+        style={{
+          top: `${12 - (mouse.y * 100) / windowSize.height / 100}%`,
+          left: `${12 - (mouse.x * 100) / windowSize.width / 100}%`,
+        }}
+      >
         <img src={blopGreenLg} alt="blop" />
       </div>
-      <div className="blop__container blop-md">
-        <img src={blopGreenMd} alt="blop" />
+      <div
+        className="blop__container blop-md"
+        style={{
+          top: `${10 + (mouse.y * 100) / windowSize.height / 100}%`,
+          left: `${10 + (mouse.x * 100) / windowSize.width / 100}%`,
+        }}
+      >
+        <img
+          src={blopGreenMd}
+          alt="blop"
+          style={{
+            backgroundShadow: `${mouse.x / windowSize.width}px ${
+              mouse.y / windowSize.height
+            }px 0px 0px rgba(0,0,0,0.5)`,
+          }}
+        />
       </div>
-      <div className="blop__container blop-sm">
+      <div
+        className="blop__container blop-sm"
+        style={{
+          top: `${8 + (mouse.y * 100) / windowSize.height / 100}%`,
+          left: `${10 - (mouse.x * 100) / windowSize.width / 50}%`,
+        }}
+      >
         <img src={blopGreenSm} alt="blop" />
       </div>
     </div>
