8e58a533abf188dfc020dfad4e1637b7fe86546e
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-12 00:37:23 +0200

    Custom Input done

--- src/components/CustomInput/CustomInput.js
+++ src/components/CustomInput/CustomInput.js
@@ -1,14 +1,26 @@
 import react from "react";
 import "../../dist/css/CustomInput.css";
 
-export default function CustomInput() {
+export default function CustomInput({ onChange, onClick }) {
   let inputStyle = {
     background: "grey.dark_hover",
   };
 
   return (
     <>
-      <input type="text" className="custom__input" style={inputStyle} />
+      <input
+        type="text"
+        className="custom__input"
+        style={inputStyle}
+        placeholder="Input here"
+        onChange={onChange}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            onClick(e.target.value);
+            e.target.value = "";
+          }
+        }}
+      />
     </>
   );
 }
