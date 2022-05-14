0acdcace43d81ca249eef31ea92f64f835020af2
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-14 16:14:51 +0200

    correct/incorrect feature added to input component

--- src/components/CustomInput/CustomInput.js
+++ src/components/CustomInput/CustomInput.js
@@ -1,15 +1,32 @@
-import react from "react";
+import react, { useState } from "react";
 import "../../dist/css/CustomInput.css";
 
-export default function CustomInput({ onClick }) {
+export default function CustomInput({ onClick, currentWord }) {
+  let [inputClasses, setInputClasses] = useState(["custom-input"]);
+
+  function displayStatus(e) {
+    console.log("e.target.value", e.target.value, "word", currentWord);
+
+    if (e.target.value === currentWord) {
+      setInputClasses(["custom-input", "custom-input-correct"]);
+    } else {
+      setInputClasses(["custom-input", "custom-input-incorrect"]);
+    }
+
+    setTimeout(() => {
+      setInputClasses(["custom-input"]);
+    }, 500);
+  }
+
   return (
     <>
       <input
         type="text"
-        className="custom__input"
+        className={inputClasses.join(" ")}
         placeholder="Input here"
         onKeyDown={(e) => {
           if (e.key === "Enter") {
+            displayStatus(e);
             onClick(e);
           }
         }}
