370263b73f0e284eef819e98aef069261e844dcf
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-16 00:10:20 +0200

    some comments

--- src/components/CustomBtn/CustomBtn.js
+++ src/components/CustomBtn/CustomBtn.js
@@ -4,6 +4,8 @@ import "../../dist/css/CustomBtn.css";
 export default function CustomBtn({ onClick, content, variant }) {
   let [btnClasses, setBtnClasses] = useState(["custom-btn"]);
 
+  //   ! for some reson, the app crashes when this code is uncommented
+  //   ! i need it to develop a better styling for the buttons
   //   if (variant === "primary") {
   //     setBtnClasses(["custom-btn", "custom-btn-prymary"]);
   //   }
