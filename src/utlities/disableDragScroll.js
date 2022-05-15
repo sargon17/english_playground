800dffe1f4f3beb1a0a36e50b35c934c61c3a1c9
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-15 14:19:22 +0200

    Update disableDragScroll.js

--- src/utlities/disableDragScroll.js
+++ src/utlities/disableDragScroll.js
@@ -1,10 +1,13 @@
 // it disable the scroll on drag, but it probably disable it at all, so probably, if it's gonna be more
 //  game tiles, the application does not scroll too.
+import checkScrollPosition from "./checkScrollPosition";
 export default function disableDragScroll() {
   document.body.addEventListener(
     "touchmove",
     function (e) {
       e.preventDefault();
+      // check if the page scrolled after inputing a word
+      checkScrollPosition();
     },
     { passive: false }
   );
