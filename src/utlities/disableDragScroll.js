aa0f06613cf29af302d6756849c7f23cea2d7185
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-15 12:32:53 +0200

    added comments to disableDragScroll function

--- src/utlities/disableDragScroll.js
+++ src/utlities/disableDragScroll.js
@@ -1,3 +1,5 @@
+// it disable the scroll on drag, but it probably disable it at all, so probably, if it's gonna be more
+//  game tiles, the application does not scroll too.
 export default function disableDragScroll() {
   document.body.addEventListener(
     "touchmove",
