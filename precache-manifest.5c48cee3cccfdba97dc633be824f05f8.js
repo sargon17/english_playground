97a0e62f5af96089f8f6136b81491df70b1bea3b
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-18 00:23:42 +0200

    Updates

---
+++ precache-manifest.5c48cee3cccfdba97dc633be824f05f8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "4eb931cab209aa1d112a",
+    "url": "/english_playground/static/css/main.b173b7f4.chunk.css"
+  },
+  {
+    "revision": "4eb931cab209aa1d112a",
+    "url": "/english_playground/static/js/main.4eb931ca.chunk.js"
+  },
+  {
+    "revision": "03762b6cad994b8dd370",
+    "url": "/english_playground/static/js/1.03762b6c.chunk.js"
+  },
+  {
+    "revision": "61826e44fab192fca141",
+    "url": "/english_playground/static/js/2.61826e44.chunk.js"
+  },
+  {
+    "revision": "383fe2544e3a2e98f27c",
+    "url": "/english_playground/static/js/runtime~main.383fe254.js"
+  },
+  {
+    "revision": "fd816822162553bb3a7993b3eccca9f6",
+    "url": "/english_playground/static/media/check-circle-fill.fd816822.svg"
+  },
+  {
+    "revision": "6dc8f4900edfdc7480b9def268691e18",
+    "url": "/english_playground/static/media/x-circle-fill.6dc8f490.svg"
+  },
+  {
+    "revision": "4fbb6ff04759a9504f7b29bb4d04eb54",
+    "url": "/english_playground/static/media/blop-red-lg.4fbb6ff0.svg"
+  },
+  {
+    "revision": "3ac64b2531c185b7ceb75fa6700bed3a",
+    "url": "/english_playground/static/media/blop-red-md.3ac64b25.svg"
+  },
+  {
+    "revision": "23eee6640a3def0c1d2a08458981c153",
+    "url": "/english_playground/static/media/blop-red-sm.23eee664.svg"
+  },
+  {
+    "revision": "4d7da1ebc15f87f74bf373620625c2fc",
+    "url": "/english_playground/static/media/blop-green-lg.4d7da1eb.svg"
+  },
+  {
+    "revision": "2ba69cc73b6acf77b2946b253a35a81d",
+    "url": "/english_playground/static/media/blop-green-md.2ba69cc7.svg"
+  },
+  {
+    "revision": "2a8066a28fc57712961806c5f6703d37",
+    "url": "/english_playground/static/media/blop-green-sm.2a8066a2.svg"
+  },
+  {
+    "revision": "7ad3038be0c95f4c9b3421da52be9ecb",
+    "url": "/english_playground/index.html"
+  }
+];
