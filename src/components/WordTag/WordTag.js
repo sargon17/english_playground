e2bde29e432ab88a5c6b280302ea223fb023c9de
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-21 00:31:11 +0200

    added the closing icon to tag on hover

--- src/components/WordTag/WordTag.js
+++ src/components/WordTag/WordTag.js
@@ -1,9 +1,16 @@
 import React from "react";
 import "../../dist/css/WordTag.css";
+import CancelIcon from "@mui/icons-material/Cancel";
 
 export default function WordTag({ word, onClick }) {
   return (
     <div className="word-tag" onClick={onClick}>
+      <CancelIcon
+        sx={{
+          color: "grey.light_hover",
+        }}
+        classes={{ root: "cancel-icon" }}
+      />
       <span>{word}</span>
     </div>
   );
