86d4e9c1d9960f3e2f8acc49d2ed9c5539f6040d
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-21 00:00:11 +0200

    notification icons added

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "../../dist/css/Notification.css";
 
+import DoneAllIcon from "@mui/icons-material/DoneAll";
+import ReportProblemIcon from "@mui/icons-material/ReportProblem";
+import ErrorIcon from "@mui/icons-material/Error";
+import CampaignIcon from "@mui/icons-material/Campaign";
+
 export default function Notification({ message, type, position, duration }) {
   let [notificationClasses, setNotificationClasses] = useState([
     "notification",
@@ -17,8 +22,46 @@ export default function Notification({ message, type, position, duration }) {
     );
   }, []);
 
+  function setIcon() {
+    switch (type) {
+      case "success":
+        return (
+          <DoneAllIcon
+            sx={{
+              color: "#fff",
+            }}
+          />
+        );
+      case "error":
+        return (
+          <ErrorIcon
+            sx={{
+              color: "#fff",
+            }}
+          />
+        );
+      case "warning":
+        return (
+          <ReportProblemIcon
+            sx={{
+              color: "#fff",
+            }}
+          />
+        );
+      default:
+        return (
+          <CampaignIcon
+            sx={{
+              color: "#fff",
+            }}
+          />
+        );
+    }
+  }
+
   return (
     <div className={notificationClasses.join(" ")}>
+      <div className="notification-icon">{setIcon()}</div>
       <div className="notification-content">
         <p className="notification-text">{message}</p>
       </div>
