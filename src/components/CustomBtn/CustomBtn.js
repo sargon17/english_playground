41d0bb6c6ff167f856d381d374c2ee075a2f18d7
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2022-05-28 00:43:19 +0200

    upd btn component classes

--- src/components/CustomBtn/CustomBtn.js
+++ src/components/CustomBtn/CustomBtn.js
@@ -1,17 +1,26 @@
 import React, { useState } from "react";
 import "../../dist/css/CustomBtn.css";
 
-export default function CustomBtn({ onClick, content, variant }) {
-  let [btnClasses, setBtnClasses] = useState(["custom-btn"]);
+export default function CustomBtn({ onClick, content, variant, customStyle }) {
+  // let [btnClasses, setBtnClasses] = useState(["custom-btn"]);
 
   //   ! for some reson, the app crashes when this code is uncommented
   //   ! i need it to develop a better styling for the buttons
-  //   if (variant === "primary") {
-  //     setBtnClasses(["custom-btn", "custom-btn-prymary"]);
-  //   }
+  // if (variant === "primary") {
+  //   setBtnClasses((prev) => [...prev, "custom-btn-prymary"]);
+  // }
+
+  let btnClasses = ["custom-btn"];
+  if (variant) {
+    btnClasses.push(`custom-btn-${variant}`);
+  }
 
   return (
-    <button className={btnClasses.join(" ")} onClick={onClick}>
+    <button
+      className={btnClasses.join(" ")}
+      style={customStyle}
+      onClick={onClick}
+    >
       {content}
     </button>
   );
